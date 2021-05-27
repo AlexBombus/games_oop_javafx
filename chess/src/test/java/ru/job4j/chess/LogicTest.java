5287bc2b9c86214fefdd23142b088d4fab5402a3
AlexBombus <78620532+AlexBombus@users.noreply.github.com>
2021-05-27 16:34:42 +0300

    2. Каркас шахматной доски [#792]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -7,12 +7,36 @@ import ru.job4j.chess.firuges.black.BishopBlack;
 
 public class LogicTest {
 
-    @Ignore
     @Test
     public void move()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
         logic.move(Cell.C1, Cell.H6);
+    }
+
+    @Test(expected = FigureNotFoundException.class)
+    public void figureNotFound()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.D1));
+        logic.move(Cell.C1, Cell.H6);
+    }
+
+    @Test(expected = OccupiedCellException.class)
+    public void cellIsOccupied()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        logic.add(new BishopBlack(Cell.B2));
+        logic.move(Cell.C1, Cell.A3);
+    }
+
+    @Test(expected = ImpossibleMoveException.class)
+    public void moveIsImpossible()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        logic.move(Cell.C1, Cell.C6);
     }
 }
--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -1,20 +1,45 @@
 package ru.job4j.chess.firuges.black;
 
-import junit.framework.TestCase;
 import org.junit.Assert;
+import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 
-public class BishopBlackTest extends TestCase {
+public class BishopBlackTest {
 
+    @Test
     public void testPosition() {
         BishopBlack bishopBlack = new BishopBlack(Cell.A1);
         Assert.assertEquals(bishopBlack.position(), Cell.A1);
     }
 
+    @Test
     public void testCopy() {
         BishopBlack bishopBlack = new BishopBlack(Cell.A1);
          Cell expected = bishopBlack.copy(Cell.A2).position();
         Assert.assertEquals(expected, Cell.A2);
     }
+
+    @Test
+    public void testIsDiagonalTrue() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.D4);
+        Cell dest = Cell.G7;
+        Assert.assertTrue(bishopBlack.isDiagonal(bishopBlack.position(), dest));
+
+    }
+
+    @Test
+    public void testIsDiagonalFalse() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.D4);
+        Cell dest = Cell.G6;
+        Assert.assertFalse(bishopBlack.isDiagonal(bishopBlack.position(), dest));
+
+    }
+
+    @Test
+    public void way() {
+        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
+        Assert.assertArrayEquals(new Cell[] {Cell.D2, Cell.E3, Cell.F4, Cell.G5}, bishopBlack.way(Cell.G5));
+    }
+
 }
 
